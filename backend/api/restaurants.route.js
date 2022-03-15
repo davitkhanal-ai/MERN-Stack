79a33fe808ba9f88ac9e54e393ff031ccc2c63a6
davitkhanal <khanaldavit@gmail.com>
2022-03-15 18:11:25 +0545

    mongodb DAO added

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import RestaurantsController from "./restaurants.controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req,res) =>{
-    res.send("Hello world")
-})
+router.route("/").get(RestaurantsController.apiGetRestaurants)
 
 export default router
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 dotenv.config()
 
 const MongoClient = mongodb.MongoClient
@@ -11,13 +12,15 @@ MongoClient.connect(
     process.env.MONGO_URI,
     {
         useNewUrlParser: true, 
-        useUnifiedTopology: true, }
+        useUnifiedTopology: true, },
     )
+
     .catch(err => {
         console.error(err.stack)
         process.exit(1)
       })
     .then( async client =>{
+        await RestaurantsDAO.injectDB(client)
         app.listen(port, ()=>{
             console.log(`listening on port ${port}`)
         })
