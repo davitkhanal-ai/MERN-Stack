ac5ab78ec17f8ba542c65a487a1df846874ec860
davitkhanal <khanaldavit@gmail.com>
2022-03-15 23:21:10 +0545

    adding basic routes section

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function AddReview() {
+  return (
+    <div className="app">
+     
+    </div>
+  );
+}
+
+export default AddReview;
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Restaurants() {
+  return (
+    <div className="app">
+     Hello world
+    </div>
+  );
+}
+
+export default Restaurants;
